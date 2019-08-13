0418978a9012b169d7541140ada2f5018b9f8a46
Your Name <you@example.com>
2019-08-13 20:20:01 -0300

    Updates

--- src/_scripts/case.js
+++ src/_scripts/case.js
@@ -4,11 +4,12 @@
 var Case = function() {
     //headings fnuctionality
     var controls = $('.brand-power-case__header__control');
-    var sections = $('[data-content]');
+    var sections = $('.brand-power-case__container');
+
+    var sectionWidth = (controls.length * 0.66);
 
     controls.on('click', function() {
-        
-        sections.addClass('-active');
+        sections.addClass('-move');
     })
 
 
